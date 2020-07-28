f234880f1aa63a4775f4a3c1ff1695e2df8120be
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-07-28 09:55:28 +0100

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tiagocoutinho/sinstruments',
-    version='1.0.0',
+    version='1.1.0',
     zip_safe=False,
         entry_points={
         'console_scripts': [
